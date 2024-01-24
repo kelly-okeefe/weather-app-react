fd284494d751faece97297ca41f764313c071687
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2024-01-24 19:35:41 +0100

    Copied code from CodeSandbox, added footer text.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Weather from './Weather';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Weather App</h1>
+        <Weather/>
       </header>
+      <div className='Footer'>
+        This project was coded by Kelly O'Keefe. It is open-sourced on GitHub, and hosted on Netlify.
+      </div>
     </div>
   );
 }
