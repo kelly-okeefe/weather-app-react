f2ec883fc3f5a00044ca1a3c4b53c09a28d4ceee
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2024-01-28 14:38:53 +0100

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
+        <div className='Container'>
+        <h1>Weather App</h1>
         <Weather/>
-      </header>
-      <div className='Footer'>
+      <footer>
         This project was coded by <a href="https://github.com/kelly-okeefe" target='_blank' rel="noreferrer" title="Go to GitHub profile">Kelly O'Keefe</a>. 
         It is open-sourced on <a href="https://github.com/kelly-okeefe/weather-app-react" target="_blank" rel="noreferrer" title="Go to the GitHub repository">GitHub</a>, and 
         hosted on <a href="https://astonishing-babka-1e8963.netlify.app/" target='_blank' rel="noreferrer" title="Launch project">Netlify</a>.
-      </div>
+        </footer>
+    </div>
     </div>
   );
 }
