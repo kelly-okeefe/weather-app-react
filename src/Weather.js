30ae4c27121a52566e1bcd17d79e73349fb5aede
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2024-02-04 12:24:14 +0100

    Added weather forecast component and CSS.

--- src/Weather.js
+++ src/Weather.js
@@ -43,10 +43,10 @@ if (weatherData.ready) {
     <form onSubmit={handleSubmit}>
       <div className="row">
       <div className="col-9" >    
-      <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" onChange={handleCityChange}/>
+      <input type="search" placeholder="Enter a city..." className="form-control no-border" autoFocus="on" onChange={handleCityChange}/>
       </div> 
       <div className="col-3">
-      <input type="submit" value="Search" className="btn btn-danger w-100"/>
+      <input type="submit" value="Search" className="btn btn-info w-100"/>
       </div>
       </div>
     </form>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,32 @@
 import React from "react";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
-    return "Hello, this is the weather forecast";
+    return (
+        <div className="WeatherForecast">
+        <div className="row">
+        <div className="col">
+            <div className = "WeatherForecast-day">
+            Thu
+            </div>
+            <div className="WeatherForecast-icon">
+            icon
+            </div>
+            <div className="WeatherForecast-temperatures">
+                <span className="WeatherForecast-temperature-max">
+            19°
+            </span>
+            <span className="WeatherForecast-temperature-min">
+            10°
+            </span>
+            </div>
+        </div>
+
+
+
+        
+        </div>
+        </div>
+    );
 }
+
